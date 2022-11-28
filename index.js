b266025075340192e6db5a167fd2edce8e67eed6
samethozman <samet.hozman@outlook.com>
2022-11-28 13:05:15 +0300

    Completed Ass,gnment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    const messages = [];
+    for(let i = 0;  i < 3; i++ ){
+        const writeCards = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(writeCards);
+    }
+return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+//function for countdown 
+
+function countDown(){
+let i = 10;
+while(i >= 0){
+    console.log(i);
+    i--;
+}
+}
+
+countDown();
